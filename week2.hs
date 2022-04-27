1fe7efd9130bb9db96314d440dbd8bc627292aae
dnszlr <dennis.zhukki@gmail.com>
2022-04-27 13:59:23 +0200

    final commit

--- week2.hs
+++ week2.hs
@@ -2,3 +2,10 @@
 add (x,y) = x + y
 -- a -> (a -> a)
 add2 x y = x + y
+
+
+quicksort [] = []
+quicksort(x:xs) = quicksort lesser ++ [x] ++ quicksort greater
+    where
+        lesser = filter (<x) xs
+        greater = filter(>= x) xs
